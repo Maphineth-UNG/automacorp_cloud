28cfedb4ec80525f9038d0c993065faa4748a3b4
maphineth <maphinethung@gmail.com>
2024-10-25 14:52:45 +0200

    modified SpringSecurityConfig with adding SecurityFilterChain

--- src/main/java/com/emse/spring/automacorp/security/SpringSecurityConfig.java
+++ src/main/java/com/emse/spring/automacorp/security/SpringSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -37,5 +38,18 @@ public class SpringSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    @Order
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).hasRole(ROLE_USER) //
+                        .anyRequest().permitAll() //
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .build();
     }
 }
