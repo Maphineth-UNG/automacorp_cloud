809d8085cc5a01837712e424a4d879f7157424d2
maphineth <maphinethung@gmail.com>
2024-10-25 15:00:07 +0200

    adding SecurityController

---
+++ src/main/java/com/emse/spring/automacorp/api/SecurityController.java
@@ -0,0 +1,22 @@
+package com.emse.spring.automacorp.api;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/admin/users")
+public class SecurityController {
+    public record User(String username) {
+    }
+
+    @GetMapping(path = "/me")
+    public User findUserName(@AuthenticationPrincipal UserDetails userDetails) {
+        return new User(userDetails.getUsername());
+    }
+}
